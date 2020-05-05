7f62e36f5c5b93fa9f38719b53314e0d9d030369
sina68 <sina.gh.68@gmail.com>
2020-05-05 06:05:38 +0430

    2

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 //Launch listening server on port 8080 and consoles the log.
-app.listen(8000, function () {
-  console.log('app listening on port 8080!')
+app.listen(3000, function () {
+  console.log('app listening on port 3000!')
 })
