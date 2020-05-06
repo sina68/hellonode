9667d346bb716dafe892310199bd2888fa2d2ce0
sina68 <sina.gh.68@gmail.com>
2020-05-06 19:37:13 +0430

    1

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-//now it load express module with `require` directive
-var express = require('express')
-var app = express()
-//Define request response in root URL (/) and response with text Hello World!
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const db = require('./queries')
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/hello', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
 })
-//Launch listening server on port 8080 and consoles the log.
-app.listen(8080, function () {
-  console.log('app listening on port 8080!')
+
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
 })
