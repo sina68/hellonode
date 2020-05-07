a0dc4fc2116effa8186540501606881100ee8e7d
sina68 <sina.gh.68@gmail.com>
2020-05-07 08:09:50 +0430

    1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const db = require('./queries')
-const port = 80
+const port = 3000
 
 app.use(bodyParser.json())
 app.use(
